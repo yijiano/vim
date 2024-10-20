3ea3c60e54bb08d7f744610a4df2531f63bcf082
Yijian <132629001+yijiano@users.noreply.github.com>
2024-10-20 20:30:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ echo Running run script...
 # Import molokai.vim
 echo Adding Molokai theme...
 mkdir -p ~/.vim/colors
-cp files/molokai.vim ~/.vim/colors/molokai.vim
+cp ./files/molokai.vim ~/.vim/colors/molokai.vim
 
 # Copy old .vimrc
 echo Backing up old .vimrc file...
-cp ~/.vimrc files/.vimrc_old
+cp ~/.vimrc ./files/.vimrc_old
 
 # Replace old .vimrc with imported version
 echo Importing new .vimrc file...
-cp files/.vimrc ~/.vimrc
+cp ./files/.vimrc ~/.vimrc
 
 echo Done!
